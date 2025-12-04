8d820c4fc12c167e89c005127659f829c648b157
Valentin Chiriac <Valentin.Chiriac@endava.com>
2025-12-04 12:02:43 +0200

    hovering, doble click, drag and drop actions

--- test/specs/advanced-element-interaction.spec.js
+++ test/specs/advanced-element-interaction.spec.js
@@ -37,7 +37,7 @@ describe('advanced element interactions - examples', () => {
         //await browser.pause(2000);
     });
 
-    it.only('state commands', async() => {
+    it('state commands', async() => {
         await browser.url("/Dropdown-Checkboxes-RadioButtons/index.html");
 
         const lettuceRadioButton = await $('[value="lettuce"]');
@@ -55,5 +55,49 @@ describe('advanced element interactions - examples', () => {
 
         const cabbageRadioButton_isClickable = await cabbageRadioButton.isClickable();
         await expect(cabbageRadioButton_isClickable).toEqual(false);
+        
+    });
+    it('actions on the web page', async () => {
+        await browser.url("/Actions/index.html");
+
+        //Drag and Drop
+        const elementToBeDragged = await $('#draggable');
+        const targetElement = await $('#droppable');
+        await elementToBeDragged.dragAndDrop(targetElement);
+        await browser.pause(2000);
+
+        //Double Click
+        const doubleClick = await $('#double-click');
+        await doubleClick.doubleClick();
+        await browser.pause(3000);
+
+        //Mouse over and element
+        await $("//button[text()='Hover Over Me First!']").moveTo();
+        const firstLink = await $("//div[@class='dropdown hover']//a[@class='list-alert'][normalize-space()='Link 1']");
+        await firstLink.waitForClickable();
+        await firstLink.click();        
+        await browser.pause(3000);
+
+        await $("//button[text()='Hover Over Me Second!']").moveTo();
+        const secondLink = await $("(//*[text()='Link 1'])[2]");
+        await secondLink.waitForClickable();
+        await secondLink.click();
+        await browser.pause(3000);
+
+        await $("//button[text()='Hover Over Me Third!']").moveTo();
+        const thirdLink = await $("(//*[text()='Link 1'])[3]");
+        await thirdLink.waitForClickable();
+        await thirdLink.click();
+        await browser.pause(3000)
+
+    });
+
+    it.only('handling multiple web windows', async() => {
+        await browser.url("https://webdriveruniversity.com/");
+        await browser.newWindow("https://automationteststore.com/");
+
+        let currentWindowTitle = await browser.getTitle();
+        console.log(`>> Title of the current window is: ${currentWindowTitle}`);
+        await browser.pause(3000);
     });
 });
